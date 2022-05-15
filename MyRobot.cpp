ca991db67fe67f81d1e934486a4d11272dcdd05e
Tom-Pacaud <103312050+Tom-Pacaud@users.noreply.github.com>
2022-05-15 22:26:20 +0200

    Premier Commit
    Communication avec le robot fonctionnel
    Travail en cours : faire fonctionner les roues du robot

---
+++ MyRobot.cpp
@@ -0,0 +1,94 @@
+// myrobot.cpp
+
+#include "myrobot.h"
+
+short Crc16(unsigned char *Adresse_tab , unsigned char Taille_max)
+{
+unsigned int Crc = 0xFFFF;
+unsigned int Polynome = 0xA001;
+unsigned int CptOctet = 0;
+unsigned int CptBit = 0;
+unsigned int Parity= 0;
+ Crc = 0xFFFF;
+ Polynome = 0xA001;
+for ( CptOctet= 1 ; CptOctet < Taille_max ; CptOctet++)
+ {
+ Crc ^= *( Adresse_tab + CptOctet);
+ for ( CptBit = 0; CptBit <= 7 ; CptBit++)
+ {
+ Parity= Crc;
+ Crc >>= 1;
+ if (Parity%2 == true) Crc ^= Polynome;
+ }
+ }
+return(Crc);
+}
+
+MyRobot::MyRobot(QObject *parent) : QObject(parent) {
+    DataToSend.resize(9);
+    DataToSend[0] = 0xFF;
+    DataToSend[1] = 0x07;
+    DataToSend[2] = 120;
+    DataToSend[3] = 120;
+    DataToSend[4] = 120;
+    DataToSend[5] = 120;
+    DataToSend[6] = 80;
+    unsigned char *in = (DataToSend);
+    short mycrc = Crc16(DataToSend,7);
+    DataToSend[7] = mycrc;
+    DataToSend[8] = (mycrc  >> 8);
+    DataReceived.resize(21);
+    TimerEnvoi = new QTimer();
+    // setup signal and slot
+    connect(TimerEnvoi, SIGNAL(timeout()), this, SLOT(MyTimerSlot())); //Send data to wifibot timer
+}
+
+
+void MyRobot::doConnect() {
+    socket = new QTcpSocket(this); // socket creation
+    connect(socket, SIGNAL(connected()),this, SLOT(connected()));
+    connect(socket, SIGNAL(disconnected()),this, SLOT(disconnected()));
+    connect(socket, SIGNAL(bytesWritten(qint64)),this, SLOT(bytesWritten(qint64)));
+    connect(socket, SIGNAL(readyRead()),this, SLOT(readyRead()));
+    qDebug() << "connecting..."; // this is not blocking call
+    //socket->connectToHost("LOCALHOST", 15020);
+    socket->connectToHost("192.168.1.106", 15020); // connection to wifibot
+    // we need to wait...
+    if(!socket->waitForConnected(5000)) {
+        qDebug() << "Error: " << socket->errorString();
+        return;
+    }
+    TimerEnvoi->start(75);
+
+}
+
+void MyRobot::disConnect() {
+    TimerEnvoi->stop();
+    socket->close();
+}
+
+void MyRobot::connected() {
+    qDebug() << "connected..."; // Hey server, tell me about you.
+}
+
+void MyRobot::disconnected() {
+    qDebug() << "disconnected...";
+}
+
+void MyRobot::bytesWritten(qint64 bytes) {
+    qDebug() << bytes << " bytes written...";
+}
+
+void MyRobot::readyRead() {
+    qDebug() << "reading..."; // read the data from the socket
+    DataReceived = socket->readAll();
+    emit updateUI(DataReceived);
+    qDebug() << DataReceived[0] << DataReceived[1] << DataReceived[2];
+}
+
+void MyRobot::MyTimerSlot() {
+    qDebug() << "Timer...";
+    while(Mutex.tryLock());
+    socket->write(DataToSend);
+    Mutex.unlock();
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "MyRobot.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    Robot.doConnect();
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    Robot.disConnect();
+}
+
